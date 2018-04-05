f489f0a2f768e8c27d75cfc8632bb593b3d8f6a2
MateusAlissonCastro <mateusalisson@gmail.com>
2018-04-05 11:11:10 -0400

    fim da aula4

--- meublog/meublog/settings.py
+++ meublog/meublog/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'posts',
+]  
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -47,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+]     
 
 ROOT_URLCONF = 'meublog.urls'
 
